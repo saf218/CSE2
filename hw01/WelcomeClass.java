3ec3844724c3b624a603c269d1b0e5dba6d8dd45
saf218 <saf218@lehigh.edu>
2014-09-02 13:25:49 +0000

    Added escape characters and compiled

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+
+//Siarra Fiol
+//Homework 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+ public static void main(String[] args) {
+     
+//  print the statement with the box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-S--A--F--2--1--8-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("v v v v v v");
+    System.out.println("This summer I went to three concerts with my friends. I enjoy all types of music genres and hope to see more shows soon.");
+    
+    //test/testtest
+  }
+}
