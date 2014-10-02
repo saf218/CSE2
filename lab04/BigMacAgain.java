2658bcebc1781cf4fe306e399f01d6685958d272
saf218 <saf218@lehigh.edu>
2014-10-02 14:41:34 +0000

    Lab04

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -9,7 +9,7 @@
 //  run the program
 //      java BigMacAgain
 
-import java.util.Scanner;
+import java.util.Scanner; //import scanner
 
 //  define a class
 public class BigMacAgain{
@@ -17,37 +17,77 @@ public class BigMacAgain{
         //  add main method
         public static void main(String[] args){
             
-            Scanner myScanner;
-            myScanner = new Scanner( System.in );
+            Scanner myScanner; //declare scanner
+            myScanner = new Scanner( System.in ); //construct scanner
             
             System.out.print(
-            "Enter the number of Big Macs(an integer > 0): ");
+            "Enter the number of Big Macs(an integer > 0): "); //prompts user interaction
             
-            int nBigMacs = myScanner.nextInt();
-            
-            //amount of good purchased
-            int amntBigMacs = myScanner.nextInt();
-            //costs of goods
+            if (myScanner.hasNextInt()) {
+                int nBigMacs = myScanner.nextInt(); 
+             
+             //amount of good purchased
+            int amntBigMacs = nBigMacs;
+            //cost of good
             double bigMacCost$ = 2.22;
-            double friesCost$ = 2.15;
             //calculations
             double totalPriceBigMacs = amntBigMacs*bigMacCost$;
+           
             
-            if(myScanner.hasNextInt() > 0){
-                System.out.print("You ordered "+amntBigMacs+" Big Macs for a cost of "+amntBigMacs+"x"+bigMacCost$+" = "+totalPriceBigMacs+".");
+            if (nBigMacs > 0) {
+                System.out.println("You ordered "+amntBigMacs+" Big Mac(s) for a cost of "+amntBigMacs+"x"+bigMacCost$+" = "+totalPriceBigMacs);
             }
-            else{
-                System.out.println("You did not enter an int");
-                return;     //allows the program to run
+            else {
+                System.out.println("You did not enter an int > 0");
+                    return;     //allows the program to run
                                 //terminates the program
-                                
             }
             
             
             
+            System.out.print(
+                "Do you want an order of fries (Y/y/N/n)? "); //prompts user interaction
+                
+            
+            String fryOrder = myScanner.next(); //fryOrder string variable
+                
+            //cost of good
+            double friesCost$ = 2.15;
+            double totalCost$ = friesCost$+totalPriceBigMacs;
+            
+            
+            if (fryOrder.equals ("Y")) {
+                System.out.println("You ordered fries at a cost of $2.15"); //if yes for fries, gives cost of just fries
+                System.out.println("The total cost of the meal is $"+totalCost$); //if yes for fries, gives total cost of meal
+            }
+            else if (fryOrder.equals ("y")) {
+                System.out.println("You ordered fries at a cost of $2.15");
+                System.out.println("The total cost of the meal is $"+totalCost$);
+            }
+            else if (fryOrder.equals ("N")) {
+                System.out.println("The total cost of the meal is $"+totalPriceBigMacs); //if no for fries, gives cost of just Big Mac order
+            }
+            else if (fryOrder.equals ("n")) {
+                System.out.println("The total cost of the meal is $"+totalPriceBigMacs);
+            }
+            else {
+                System.out.println("You did not enter one of 'y', 'Y', 'N', 'n'"); //user did not enter valid choice
+            }
+            }
+            else {
+                System.out.println("You did not enter an int"); //user did not enter int
+            }
+            
+            
+                
+            
+           
+             
             
             
             
+            
+        }        
             
 }
-    }
+    
